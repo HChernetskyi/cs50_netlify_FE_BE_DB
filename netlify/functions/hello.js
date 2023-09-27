383c26aeba9d74c551bae2765b4c0cf0bf59f2a2
HChernetskyi <94737464+HChernetskyi@users.noreply.github.com>
2023-09-27 16:33:46 +0300

    Create hello.js

---
+++ netlify/functions/hello.js
@@ -0,0 +1,8 @@
+//functions/hello.js
+exports.handler = async (event, context) => {
+  const { name = "Anonymous" } = event.queryStringParameters;
+  return {
+    statusCode: 200,
+    body: `Hello, ${name}`
+  };
+};
