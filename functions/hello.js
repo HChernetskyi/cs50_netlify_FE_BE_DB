19eb304d3a04399009c8e07d4b30383e97e6b7b1
HChernetskyi <94737464+HChernetskyi@users.noreply.github.com>
2023-09-28 15:28:07 +0300

    Delete functions directory

--- functions/hello.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async(event, context) => {
-    const { name = "Anonymous" } = event.queryStringParameters;
-    return {
-        statusCode: 200,
-        body: `Hello, ${name}`
-    };
-};
