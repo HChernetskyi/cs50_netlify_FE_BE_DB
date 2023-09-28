379ba3d1d2d0ac83dcaca61cb658c7709fe38459
HChernetskyi <94737464+HChernetskyi@users.noreply.github.com>
2023-09-28 13:45:11 +0300

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,5 @@
 exports.handler = async(event, context) => {
     const { name = "Anonymous" } = event.queryStringParameters;
-    const path = event.path.replace(/\.netlify\/functions\/[^/]+/, '')
     return {
         statusCode: 200,
         body: `Hello, ${name}`
