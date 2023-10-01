4dc7955d9cb8ae886a742c7fbc5c55b297911fda
HChernetskyi <94737464+HChernetskyi@users.noreply.github.com>
2023-10-01 15:37:48 +0300

    Update load.js

--- netlify/functions/load.js
+++ netlify/functions/load.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 const supabase = createClient('https://ieyfgpklmrjyydtduzqd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlleWZncGtsbXJqeXlkdGR1enFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3MzM2NzAsImV4cCI6MjAxMTMwOTY3MH0.Aj88UhsBJ6NjJQW3Wfw6Z0mqfLFkkb9tIM22HYapJcI')
 const todoList = document.getElementById("todoList");
 
-const { jobs, error } = await supabase
+const { jobs, error } = supabase
   .from('jobs')
   .select()
 
