7a12c3774389be3a6e69104b3b5ed838542be95a
HChernetskyi <94737464+HChernetskyi@users.noreply.github.com>
2023-10-01 15:30:56 +0300

    Create load.js

---
+++ netlify/functions/load.js
@@ -0,0 +1,15 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const supabase = createClient('https://ieyfgpklmrjyydtduzqd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlleWZncGtsbXJqeXlkdGR1enFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3MzM2NzAsImV4cCI6MjAxMTMwOTY3MH0.Aj88UhsBJ6NjJQW3Wfw6Z0mqfLFkkb9tIM22HYapJcI')
+const todoList = document.getElementById("todoList");
+
+const { jobs, error } = await supabase
+  .from('jobs')
+  .select()
+
+renderTodos();
+
+function renderTodos() {
+  todoList.innerHTML = "";
+  todoList.innerHTML += "Hello World!";
+  }
