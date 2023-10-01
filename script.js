7f2fc51c546c139a4f3e12c0255b41d85b3d616e
HChernetskyi <94737464+HChernetskyi@users.noreply.github.com>
2023-10-01 16:42:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 renderTodos();
 
 function renderTodos() {
-  import { createClient } from "@supabase/supabase-js";
+  import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
   todoList.innerHTML += "";
   todoList.innerHTML += "Hello World!";
 }
