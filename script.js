dccc2eedeb60f49ce892c12713fd7021f3128078
HChernetskyi <94737464+HChernetskyi@users.noreply.github.com>
2023-10-01 15:07:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { createClient } from '/supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient('https://ieyfgpklmrjyydtduzqd.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlleWZncGtsbXJqeXlkdGR1enFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU3MzM2NzAsImV4cCI6MjAxMTMwOTY3MH0.Aj88UhsBJ6NjJQW3Wfw6Z0mqfLFkkb9tIM22HYapJcI')
 const todoList = document.getElementById("todoList");
